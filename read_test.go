e5a41ec336c6ce7c275423bdde07f7ca17b9264a
bran <89377210@qq.com>
2022-12-25 16:56:58 +0800

    add test

---
+++ read_test.go
@@ -0,0 +1,55 @@
+package pitaya
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestReadCsv(t *testing.T) {
+	file := "testdata/1.csv"
+	dt, err := Read(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(dt.MaxRow())
+}
+
+func TestReadDat(t *testing.T) {
+	file := "testdata/1.dat"
+	dt, err := Read(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(dt.MaxRow())
+}
+
+func TestReadTxt(t *testing.T) {
+	file := "testdata/1.txt"
+	dt, err := Read(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(dt.MaxRow())
+}
+
+func TestReadXls(t *testing.T) {
+	file := "testdata/1.xls"
+	dt, err := Read(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(dt.MaxRow())
+	dt.ForeachRow(func(i int, row *Row) bool {
+		fmt.Println(row)
+		return false
+	})
+}
+
+func TestReadXlsx(t *testing.T) {
+	file := "testdata/4.xlsx"
+	dt, err := Read(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(dt.MaxRow())
+}
